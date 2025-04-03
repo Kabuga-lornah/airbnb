488640f627976b4fce8e83040f98f6dc17ef5984
kabugalornah <wangarilornah@gmail.com>
2025-04-03 12:49:34 +0300

    new changes

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzBflo7f2uFgiFJb2ltKdMK3xTL8KDMR4",
+  authDomain: "airbnb-8bb51.firebaseapp.com",
+  projectId: "airbnb-8bb51",
+  storageBucket: "airbnb-8bb51.firebasestorage.app",
+  messagingSenderId: "674835301870",
+  appId: "1:674835301870:web:a143eccca53b9e5c22baff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
